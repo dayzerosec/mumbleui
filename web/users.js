d2703f797353247e5d99a240076c6113975dcd69
underscore-zi <__zi@0x0539.net>
2021-11-15 11:32:04 -0600

    Fix bug where speakers wouldn't get destroyed if their 'name' field was different from their mumble username

--- web/users.js
+++ web/users.js
@@ -1,21 +1,25 @@
 /* keys will only be looked up in lower-case */
 var users = {
     "zi": {
+        id: "zi",
         avatar: "assets/zi.png",
         name: "zi",
         class: undefined,
     },
     "specter": {
+        id: "specter",
         avatar: "assets/specter.png",
         name: "Specter",
         class: undefined,
     },
     "anti": {
+        id: "anti",
         avatar: "assets/anti.png",
         name: "Anti",
         class: undefined,
     },
     "_default": {
+        id: "default_user",
         avatar: "",
         name: "???",
         class: "hide",
